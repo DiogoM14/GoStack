63f8f6bb707891447c732209cebf1fc792909425
Xavi002 <diogomartins200214@gmail.com>
2020-10-07 20:07:51 +0100

    CHALLENGE 2: update

---
+++ desafios/1-conceitos-nodejs/src/app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const cors = require("cors");
+
+const { v4: uuid } = require('uuid');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const repositories = [];
+
+app.get("/repositories", (request, response) => {
+  return response.json(repositories)
+});
+
+app.post("/repositories", (request, response) => {
+  const { title, url, techs } = request.body
+
+  const repository = {id: uuid(), title, url, techs}
+
+  repositories.push(repository)
+
+  return response.json(repository)
+});
+
+app.put("/repositories/:id", (request, response) => {
+  // TODO
+});
+
+app.delete("/repositories/:id", (request, response) => {
+  // TODO
+});
+
+app.post("/repositories/:id/like", (request, response) => {
+  // TODO
+});
+
+module.exports = app;
