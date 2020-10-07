639dd7f990bd1f6e47e7dc1522921b6ea8bccd5e
Xavi002 <diogomartins200214@gmail.com>
2020-10-07 21:35:15 +0100

    CHALLENGE 2: likes

--- desafios/1-conceitos-nodejs/src/app.js
+++ desafios/1-conceitos-nodejs/src/app.js
@@ -15,9 +15,9 @@ app.get("/repositories", (request, response) => {
 });
 
 app.post("/repositories", (request, response) => {
-  const { title, url, techs } = request.body
+  const { title, url, techs, likes } = request.body
 
-  const repository = {id: uuid(), title, url, techs}
+  const repository = {id: uuid(), title, url, techs, likes: 0}
 
   repositories.push(repository)
 
@@ -33,7 +33,18 @@ app.delete("/repositories/:id", (request, response) => {
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const { likes } = request.body
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id)
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found' })
+  }
+
+  repositories[repositoryIndex].likes ++
+
+  return response.json(repositories[repositoryIndex])
 });
 
 module.exports = app;
