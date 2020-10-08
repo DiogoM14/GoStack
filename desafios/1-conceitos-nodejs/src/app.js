682bb87bf7d8cd43b64fa3a8b1573de682072bfa
Xavi002 <diogomartins200214@gmail.com>
2020-10-08 21:45:43 +0100

    CHALLENGE 2: delete and update routes

--- desafios/1-conceitos-nodejs/src/app.js
+++ desafios/1-conceitos-nodejs/src/app.js
@@ -15,7 +15,7 @@ app.get("/repositories", (request, response) => {
 });
 
 app.post("/repositories", (request, response) => {
-  const { title, url, techs, likes } = request.body
+  const { title, url, techs } = request.body
 
   const repository = {id: uuid(), title, url, techs, likes: 0}
 
@@ -25,11 +25,38 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const { title, url, techs } = request.body
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id)
+
+  if (repositoryIndex < 0) {
+    return response.status(402).json({ error: 'Repository not found.' })
+  }
+
+  repository = {
+    title,
+    url,
+    techs
+  }
+
+  repositories[repositoryIndex] = repository
+
+  return response.json(repository)
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id)
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found.' })
+  }
+
+  repositories.splice(repositoryIndex, 1)
+
+  return response.status(204).send()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
