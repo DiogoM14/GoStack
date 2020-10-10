3eb10a7f76bb83999969f8fbcba060daea11776e
Xavi002 <diogomartins200214@gmail.com>
2020-10-10 16:14:11 +0100

    FRONTEND: components

---
+++ conceitos/frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Header from './components/Header'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Header />
+      <Header />
+    </>
+  )
+}
+
+export default App
