968cbd5caec72324440f5d6414a24f71b0adae93
Xavi002 <diogomartins200214@gmail.com>
2020-11-07 15:26:03 +0000

    BACKEND: repository

--- desafios/5-fundamentos-node/src/repositories/TransactionsRepository.ts
+++ desafios/5-fundamentos-node/src/repositories/TransactionsRepository.ts
@@ -24,7 +24,31 @@ class TransactionsRepository {
   }
 
   public getBalance(): Balance {
-    // TODO
+    const { income, outcome } = this.transactions.reduce(
+      (acc: Balance, transaction: Transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            acc.income += transaction.value
+            break
+          case 'outcome':
+            acc.outcome += transaction.value
+            break
+          default:
+            break
+        }
+
+        return acc
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0
+      }
+    )
+
+    const total = income - outcome
+
+    return { income, outcome, total }
   }
 
   public create({ title, value, type }: TransactionDTO): Transaction {
